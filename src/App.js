6453eb4b31c01105182bde6e53c4776a33135a28
FocusJK <jiraphat.khupanit@gmail.com>
2018-07-24 13:38:47 +0700

    create header and edit App

--- src/App.js
+++ src/App.js
@@ -3,30 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import Main from './Main';
+import Header from './Components/Header';
+
 import GitList from './Components/GitList';
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
 
 const App = () => (
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/gitlist">Git List</Link>
-        </li>
-      </ul>
-
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/gitlist" component={GitList} />
-    </div>
-  </Router>
+  <div>
+    <Main />
+  </div>
 );
 export default App;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Menu, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+export default class Header extends Component {
+  static PropTypes = {
+    list: PropTypes.array,
+    activeItem: PropTypes.string,
+    handleItemClick: PropTypes.func,
+    isLogin: PropTypes.bool,
+    handleLogin: PropTypes.func,
+  };
+  static defaultProps = {
+    list: [{ title: 'Q&A', to: '/' }, { title: 'code', to: '/gitlist' }],
+    activeItem: 'Q&A',
+    handleItemClick: () => {},
+    isLogin: false,
+    handleLogin: () => {},
+  };
+  render() {
+    const { list, activeItem, handleItemClick, isLogin, handleLogin } = this.props;
+    return (
+      <Menu tabular>
+        {list.map(item => {
+          return (
+            <Menu.Item
+              //   key={`${menuName + path + index}`}
+              as={Link}
+              to={item.to}
+              name={item.title}
+              active={activeItem === item.title}
+              onClick={handleItemClick(item.title)}
+            />
+          );
+        })}
+        <Menu.Menu position="right">
+          <Menu.Item name={isLogin ? 'logout' : 'login'} onClick={handleLogin} />
+        </Menu.Menu>
+      </Menu>
+    );
+  }
+}
