ef1cc37b442d106177544771901a9aeb3e3e7009
Poomchai Taechaprapasrat <poomchai.ta@gmail.com>
2018-07-24 11:08:53 +0700

    remove unuse router

--- src/App.js
+++ src/App.js
@@ -3,41 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import GitList from './Components/GitList';
 const Home = () => (
   <div>
     <h2>Home</h2>
-  </div>
-);
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-);
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.path}/:topicId`} component={Topic} />
-    <Route exact path={match.path} render={() => <h3>Please select a topic.</h3>} />
   </div>
 );
 
@@ -49,18 +18,14 @@ const App = () => (
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
+          <Link to="/gitlist">Git List</Link>
         </li>
       </ul>
 
       <hr />
 
       <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <Route path="/gitlist" component={GitList} />
     </div>
   </Router>
 );
---
+++ src/Components/GitList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class GitList extends Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+export default GitList;
