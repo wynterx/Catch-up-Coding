fa41379a616deb7678ab8477a148bdfe5ac04044
wynterx <Ching@Penpichas-MacBook-Air.local>
2018-07-25 10:59:28 +0700

    edit path

--- src/View/Home.js
+++ src/View/Home.js
@@ -29,7 +29,7 @@ class Home extends Component {
   };
   render() {
     if (this.state.redirect) {
-      return <Redirect to="/main/qa" />;
+      return <Redirect to={'/main/qa/' + this.state.displayName} />;
     }
     return (
       <BgContainer mt="12%" alignItems="center" justifyContent="center">
